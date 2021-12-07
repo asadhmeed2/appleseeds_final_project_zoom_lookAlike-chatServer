de226c5192475bf06c44b6c097697ab985e28a9f
newasad <85388826+newasad@users.noreply.github.com>
2021-12-07 17:23:14 +0200

    Update chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -6,16 +6,31 @@ const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 let messages=[]
-
+let users=[];
 
 
 io.on("connection",(socket)=>{
+    socket.on("user joined",(user)=>{
+        console.log("user joined");
+        users.push({userName:user.userName,id:user.id});
+        socket.join(user.roomID)
+    })
     socket.on("get all messages",()=>{
         io.emit("all messages", messages);               
     })
     socket.on("message",({userName,message})=>{
         messages.push({userName:userName,message:message})
         io.emit("all messages", messages);               
+      })
+      socket.on("disconnect",()=>{
+          console.log("users",users);
+      let temp =users.filter((user)=> user.id!==socket.id)
+      console.log("temp" ,temp);
+      if(temp.length===0){
+          messages=[];
+      }
+      console.log("user disconnected");
+      io.emit("all messages", messages);
       })
 })
 
